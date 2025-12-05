fca6d704028cedfc67c30121f11049cfe32dd41a
muhdzaheermv <muhdzaheermv@gmail.com>
2025-12-05 11:13:11 +0530

    fevicon and logo

--- pages/index.js
+++ pages/index.js
@@ -164,7 +164,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Hikma 2025</title>
+        <title>Hikma</title>
         <meta name="description" content="Ragam 2023 | Home" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
